cd3843fbb788dc511c2686c54283eb3b53ca6f74
Yehoshua Cohen <100349784+Shuicohen@users.noreply.github.com>
2024-12-29 15:41:14 +0200

    Update LoginPage.js

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -11,7 +11,7 @@ const LoginPage = () => {
         e.preventDefault();
 
         try {
-            const response = await fetch("http://localhost:5000/login", {
+            const response = await fetch("https://sweet-tooth-lqt1.onrender.com/login", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(formData),
